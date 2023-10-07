316a663cccb2f4530ba332683d2b5cb021ba8ef0
Emma A. A. Rünzel <emma@ruenzel.de>
2023-10-07 15:00:41 -0400

    Add Examples Demonstrating Use of 'self' in Class Methods & Instances

--- lecture_notes.rb
+++ lecture_notes.rb
@@ -56,4 +56,98 @@
 
 # Class methods on classes
 # ? Self
+# self.class_method
 
+# Facebook Examples
+# class Post
+#   def self.trending_topics
+#     # code to find trending topics
+#   end
+# end
+
+# class User
+#   def self.friend_suggestions(user_id)
+#     # code to find friend suggestions for a given user
+#   end
+# end
+
+# Duolingo Examples
+# class Lesson
+#   def self.daily_target(user_id)
+#     # code to find daily target for a given user
+#   end
+# end
+
+# class User
+#   def self.leaderboard
+#     # code to generate the leaderboard
+#   end
+# end
+
+# ? Self as instance
+
+class User
+  def age=(new_age)
+    if new_age >= 0
+      @age = new_age
+    else
+      puts "Age cannot be negative."
+    end
+  end
+
+  def have_birthday
+    self.age = self.age + 1  # Calls the age= method, ensuring age cannot be negative
+  end
+end
+
+class Animal
+  def make_sound
+    "Some generic animal sound"
+  end
+
+  def describe
+    "This animal makes a #{self.make_sound}"
+  end
+end
+
+class Dog < Animal
+  def make_sound
+    "Woof!"
+  end
+end
+
+dog = Dog.new
+puts dog.describe  # Output: "This animal makes a Woof!"
+
+# This principle suggests that all services offered
+# by a module should be available through a uniform notation,
+# which does not betray whether they are implemented
+# through storage or computation. When you use self.attribute
+# instead of directly using @attribute,
+# the caller doesn't need to know whether the attribute
+# is a stored property or computed through a method;
+# they access it the same way in either case.
+
+# In summary, using self can provide greater flexibility,
+# maintainability, & readability in your code
+
+class Animal
+  def make_sound
+    "Some generic animal sound"
+  end
+
+  def describe
+    "This animal makes a #{make_sound}"
+  end
+end
+
+class Dog < Animal
+  def make_sound
+    super + ", specifically a woof!"
+  end
+end
+
+dog = Dog.new
+puts dog.describe
+# Output: "This animal makes a Some generic animal sound,
+# specifically a woof!"
