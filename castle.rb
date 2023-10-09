e9734b132575a2a8be4a0afe68591a71ef8588da
Emma A. A. Rünzel <emma@ruenzel.de>
2023-10-09 09:14:03 -0400

    Add Test Castle to castle.rb

--- castle.rb
+++ castle.rb
@@ -19,37 +19,39 @@ class Castle < Building
   end
 end
 
-# Build your own castle
-castle = Castle.new("Ahrensburger Schloss", 18, 20, "Schubiduu...uh")
-# p castle
-# p castle.butler = "James" # We can do this ebcause of our writer / accessor
-# puts "Does that castle have a butler?"
-# p castle.has_a_butler?
-puts "How big is the castle?"
-puts "- It has an area of #{castle.area} square meters 🏰"
+castle = Castle.new("Ahrensburger Schloss", 18, 20, "Schubiduu...uh", "Von Rantzau")
+p castle
+
+# # Build your own castle
+# castle = Castle.new("Ahrensburger Schloss", 18, 20, "Schubiduu...uh")
+# # p castle
+# # p castle.butler = "James" # We can do this ebcause of our writer / accessor
+# # puts "Does that castle have a butler?"
+# # p castle.has_a_butler?
+# puts "How big is the castle?"
+# puts "- It has an area of #{castle.area} square meters 🏰"
 
 
 # Real Life Example: super
+# # Normal Airbnb Flat
+#   def total_price(nights)
+#     base_price * nights + cleaning_fee
+#   end
 
-# Normal Airbnb Flat
-  def total_price(nights)
-    base_price * nights + cleaning_fee
-  end
-
-# Luxery Airbnb Flat
-  def total_price(nights)
-    super(nights) + concierge_fee
-  end
+# # Luxery Airbnb Flat
+#   def total_price(nights)
+#     super(nights) + concierge_fee
+#   end
 
 
-# Normal post
-    def display
-    puts "Caption: #{caption}"
-    puts "Image: #{image_url}"
-  end
+# # Normal post
+#     def display
+#     puts "Caption: #{caption}"
+#     puts "Image: #{image_url}"
+#   end
 
-# Post w/ add
-  def display
-    super()
-    puts "Ad: #{ad_text}"
-  end
+# # Post w/ add
+#   def display
+#     super()
+#     puts "Ad: #{ad_text}"
+#   end
