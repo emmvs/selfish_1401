313e857b6d749f8c074845c116f637eba023a24e
Emma A. A. Rünzel <emma@ruenzel.de>
2023-10-14 14:11:53 -0400

    Update Animals to Make Specific Sound

--- live_code.rb
+++ live_code.rb
@@ -1,16 +1,40 @@
-# Define an Animal with an initialize(name) constructor, exposing its name.
-
 class Animal
+  attr_reader :name
   def initialize(name)
     @name = name
   end
 end
 
-animal = Animal.new("🦥")
-p animal
+class Lion < Animal
+  def talk(name)
+    "#{name} roars"
+  end
+end
 
-# Define three Lion, Meerkat and Warthog classes
-# Implement a #talk instance method which returns a sentence like "Simba roars" for each animal. (Hint: Meerkats bark and Warthog grunt)
+class Meerkat < Animal
+  def talk(name)
+    "#{name} barks"
+  end
+end
+
+class Warthog < Animal
+  def talk(name)
+    "#{name} grunts"
+  end
+end
+
+simba = Lion.new("Simba")
+nala = Lion.new("Nala")
+timon = Meerkat.new("Timon")
+pumbaa = Warthog.new("Pumbaa")
+
+animals = [simba, nala, timon, pumbaa]
+
+animals.each do |animal|
+  puts animal.talk(animal.name)
+end
+
 # In another Ruby file, create an array with Simba, Nala, Timon & Pumbaa, iterate over it and puts the sound each animal make
+
 # Implement a ::phyla class method which will return an array of the phyla of the animal kingdom.
 # Add an #eat(food) instance method in Animal which returns a sentence like “Timon eats a scorpion”. Then override this method for the Lion class, and return a sentence like “Simba eats a gazelle. Law of the Jungle!
