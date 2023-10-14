d81cef8d3183e449b1e7a136327209de26442cf6
Emma A. A. Rünzel <emma@ruenzel.de>
2023-10-14 14:47:01 -0400

    Update Livecode w/ Class Test

--- live_code.rb
+++ live_code.rb
@@ -5,36 +5,49 @@ class Animal
   end
 end
 
-class Lion < Animal
-  def talk(name)
-    "#{name} roars"
+describe Animal do
+  describe "#initialize" do
+    it "creates an instance of an Animal" do
+      animal = Animal.new("Sloth")
+      expect(animal).to be_a(Animal)
+    end
   end
 end
 
-class Meerkat < Animal
-  def talk(name)
-    "#{name} barks"
-  end
-end
+# class Lion < Animal
+#   def talk(name)
+#     "#{name} roars"
+#   end
+# end
 
-class Warthog < Animal
-  def talk(name)
-    "#{name} grunts"
-  end
-end
+# class Meerkat < Animal
+#   def talk(name)
+#     "#{name} barks"
+#   end
+# end
 
-simba = Lion.new("Simba")
-nala = Lion.new("Nala")
-timon = Meerkat.new("Timon")
-pumbaa = Warthog.new("Pumbaa")
+# class Warthog < Animal
+#   def talk(name)
+#     "#{name} grunts"
+#   end
+# end
 
-animals = [simba, nala, timon, pumbaa]
+# simba = Lion.new("Simba")
+# nala = Lion.new("Nala")
+# timon = Meerkat.new("Timon")
+# pumbaa = Warthog.new("Pumbaa")
 
-animals.each do |animal|
-  puts animal.talk(animal.name)
-end
+# animals = [simba, nala, timon, pumbaa]
 
-# In another Ruby file, create an array with Simba, Nala, Timon & Pumbaa, iterate over it and puts the sound each animal make
+# animals.each do |animal|
+#   puts animal.talk(animal.name)
+# end
 
-# Implement a ::phyla class method which will return an array of the phyla of the animal kingdom.
-# Add an #eat(food) instance method in Animal which returns a sentence like “Timon eats a scorpion”. Then override this method for the Lion class, and return a sentence like “Simba eats a gazelle. Law of the Jungle!
+# # Implement a ::phyla class method which will return an array of the phyla of the animal kingdom.
+
+# def phyla
+#   ["Moss", "Sponge"]
+# end
+
+
+# # Add an #eat(food) instance method in Animal which returns a sentence like “Timon eats a scorpion”. Then override this method for the Lion class, and return a sentence like “Simba eats a gazelle. Law of the Jungle!
